aea249bf090063a667ffeaf9c641814d225bb467
cerbe <cerberusprogrammer@gmail.com>
2021-10-06 15:42:54 -0700

    Tooltip agregado para todos, y nuevo formato de imagenes.

--- src/Controller.java
+++ src/Controller.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -22,17 +23,28 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Pais argentina = new Pais("Agentina", "Buenos Aires", 1916, 2.78);
-        Pais bolivia = new Pais("Bolivia", "La Paz", 1825, 1.099);
-        Pais brasil = new Pais("Brasil", "Brasilia", 1822, 8.516);
-        Pais canada = new Pais("Canada", "Ottawa", 1867, 9.985);
-        Pais chile = new Pais("Chile", "Santiago de Chile", 1818, 0.756950);
-        Pais colombia = new Pais("Colombia", "Bogota", 1810, 1.143);
-        Pais costarica = new Pais("Costa Rica", "San José", 1821, 0.051180);
-        Pais cuba = new Pais("Cuba", "La Habana", 1868, 0.109884);
-        Pais estadosunidos = new Pais("Estados Unidos", "Washington", 1776, 9.834);
-        Pais mexico = new Pais("Mexico", "Ciudad de Mexico", 1810, 1.973);
-        Pais venezuela = new Pais("Venezuela", "Caracas", 1811, 0.916445);
+        Pais argentina = new Pais("Agentina", "Buenos Aires", 1916, 2.78,
+                new ImageView("img/argentina_adobespark.png"));
+        Pais bolivia = new Pais("Bolivia", "La Paz", 1825, 1.099,
+                new ImageView("img/bolivia_adobespark.png"));
+        Pais brasil = new Pais("Brasil", "Brasilia", 1822, 8.516,
+                new ImageView("img/brasil_adobespark.png"));
+        Pais canada = new Pais("Canada", "Ottawa", 1867, 9.985,
+                new ImageView("img/canada_adobespark.png"));
+        Pais chile = new Pais("Chile", "Santiago de Chile", 1818, 0.756950,
+                new ImageView("img/chile_adobespark.png"));
+        Pais colombia = new Pais("Colombia", "Bogota", 1810, 1.143,
+                new ImageView("img/colombia_adobespark.png"));
+        Pais costarica = new Pais("Costa Rica", "San José", 1821, 0.051180,
+                new ImageView("img/costarica_adobespark.png"));
+        Pais cuba = new Pais("Cuba", "La Habana", 1868, 0.109884,
+                new ImageView("img/cuba_adobespark.png"));
+        Pais estadosunidos = new Pais("Estados Unidos", "Washington", 1776, 9.834,
+                new ImageView("img/estadosunidos_adobespark.png"));
+        Pais mexico = new Pais("Mexico", "Ciudad de Mexico", 1810, 1.973,
+                new ImageView("img/mexico_adobespark.png"));
+        Pais venezuela = new Pais("Venezuela", "Caracas", 1811, 0.916445,
+                new ImageView("img/venezuela_adobespark.png"));
 
         Lista<Pais> paises = new Lista<>();
         paises.insertaInicio(argentina);
@@ -47,6 +59,6 @@ public class Controller implements Initializable {
         paises.insertaFinal(mexico);
         paises.insertaFinal(venezuela);
 
-
+        box.getItems().add(paises.getInicio().getImageView());
     }
 }
--- src/Pais.java
+++ src/Pais.java
@@ -1,3 +1,4 @@
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 
 public class Pais {
@@ -7,12 +8,16 @@ public class Pais {
     int fundacion;
     double superficie;
     ImageView imageView;
+    Tooltip tooltip;
 
-    public Pais(String pais, String capital, int fundacion, double superficie) {
+    public Pais(String pais, String capital, int fundacion, double superficie, ImageView imageView) {
         this.pais = pais;
         this.capital = capital;
         this.fundacion = fundacion;
         this.superficie = superficie;
+        this.imageView = imageView;
+        this.tooltip = new Tooltip(this.toString());
+        Tooltip.install(this.imageView, tooltip);
     }
 
     public String getPais() {
@@ -47,14 +52,27 @@ public class Pais {
         this.superficie = superficie;
     }
 
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    public Tooltip getTooltip() {
+        return tooltip;
+    }
+
+    public void setTooltip(Tooltip tooltip) {
+        this.tooltip = tooltip;
+    }
 
     @Override
     public String toString() {
-        return "Pais{" +
-                "pais='" + pais + '\'' +
-                ", capital='" + capital + '\'' +
-                ", fundacion=" + fundacion +
-                ", superficie=" + superficie +
-                '}';
+        return "Pais: " + pais + "\n" +
+                "Capital: " + capital + "\n" +
+                "Fundacion: " + fundacion + "\n" +
+                "Superficie: " + superficie;
     }
 }
